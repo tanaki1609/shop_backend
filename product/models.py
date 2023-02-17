5505f85e7b4df2f3247a447d078ba8f3f2bcecb9
tanaki <tanaki.1609@gmail.com>
2023-02-17 21:47:46 +0600

    lesson 2

--- product/models.py
+++ product/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                 null=True)
+    tags = models.ManyToManyField(Tag, blank=True)
     title = models.CharField(max_length=100)
     text = models.TextField(null=True, blank=True)
     price = models.FloatField()
@@ -14,3 +29,33 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def tag_list(self):
+        # l = []
+        # for tag in self.tags.all():
+        #     l.append(tag.name)
+        # return l
+        return [tag.name for tag in self.tags.all()]
+
+    @property
+    def filtered_reviews(self):
+        return self.product_reviews.filter(stars__gt=3)
+
+
+CHOICES = (
+    (1, '*'),
+    (2, 2 * '* '),
+    (3, 3 * '* '),
+    (4, 4 * '* '),
+    (5, 5 * '* '),
+)
+
+
+class Review(models.Model):
+    text = models.TextField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,
+                                related_name='product_reviews')
+    stars = models.IntegerField(choices=CHOICES, default=1)
+
+    def __str__(self):
+        return self.text
--- product/serializers.py
+++ product/serializers.py
@@ -1,9 +1,39 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Category, Tag, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = 'id text stars'.split()
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = 'id name'.split()
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = 'id name'.split()
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    tags = TagSerializer(many=True)
+    category_str = serializers.SerializerMethodField()
+    product_reviews = ReviewSerializer(many=True)
+    filtered_reviews = ReviewSerializer(many=True)
+
     class Meta:
         model = Product
-        fields = '__all__'
-        # exclude = ['text']
+        fields = 'id title price category tags category_str' \
+                 ' tag_list product_reviews filtered_reviews'.split()
+
+    def get_category_str(self, product):
+        try:
+            return product.category.name
+        except:
+            return 'no category!'
